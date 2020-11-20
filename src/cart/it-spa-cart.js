4604e168c5e6c6793cae4f00284c70d67cf01d9a
Nastia <56606127+Anastasiia-Malchikhina@users.noreply.github.com>
2020-11-20 10:27:45 +0100

    Update it-spa-cart.js

--- src/cart/it-spa-cart.js
+++ src/cart/it-spa-cart.js
@@ -4,18 +4,5 @@ import { Cart } from "./cart";
 export const itSpaCart = () => {
   const cart = new Cart();
   const fragment = $(new DocumentFragment());
-  // const cookieStore = document.cookie;
-  // const addRoom = $('<a href="#">Add room</a>');
-  // cookieStore.addEventListener( (event) => {
-  //     // jesli zaistniala zmiana w cookies,
-  //     // ponownie pobieram zawartosc kosza
-  //     const nowaZawartosc = cart.get();
-  //     const basket = $('<div class="cart"></div>');
-  //     basket.text(nowaZawartosc);
-  //     fragment.append(basket);
-  //     // ...i poprawiam wyswietlane przez kosz informacje
-  //     // TODO: zaktualizuj to co wyswietla obecnie koszyk
-  // });
-
-   return fragment;
+  return fragment;
 };
